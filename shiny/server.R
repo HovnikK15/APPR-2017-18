15cd6366348af05bf029d483ba728fc3ba3c35e4
Klemen Hovnik <klemen.hovnik@gmail.com>
2018-02-16 13:53:11 +0100

    dodan shiny

--- shiny/server.R
+++ shiny/server.R
@@ -1,24 +1,59 @@
 library(shiny)
+library(ggplot2)
 
 shinyServer(function(input, output) {
-  output$druzine <- DT::renderDataTable({
-    dcast(druzine, obcina ~ velikost.druzine, value.var = "stevilo.druzin") %>%
-      rename(`Občina` = obcina)
+  output$tabela2 <- renderPlot({
+    ggplot(data = tabela2 %>% filter(Drzava_drzavljanstva ==input$drzava,
+                                     Spol == input$spol),
+           aes(x = Leto, y = Stevilo)) +
+      geom_col(color = "purple", fill = "white")
+  }) 
+  output$html1 <- renderPlot({
+    ggplot(data = html1 %>% filter(Drzava_prihodnjega_prebivalisca ==input$drzava,
+                                   Spol == input$spol),
+           aes(x = Leto, y = Stevilo)) +
+      geom_col(color = "blue", fill = "white") 
   })
   
-  output$pokrajine <- renderUI(
-    selectInput("pokrajina", label="Izberi pokrajino",
-                choices=c("Vse", levels(obcine$pokrajina)))
-  )
-  output$naselja <- renderPlot({
-    main <- "Pogostost števila naselij"
-    if (!is.null(input$pokrajina) && input$pokrajina %in% levels(obcine$pokrajina)) {
-      t <- obcine %>% filter(pokrajina == input$pokrajina)
-      main <- paste(main, "v regiji", input$pokrajina)
-    } else {
-      t <- obcine
-    }
-    ggplot(t, aes(x = naselja)) + geom_histogram() +
-      ggtitle(main) + xlab("Število naselij") + ylab("Število občin")
-  })
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#library(shiny)
+
+#shinyServer(function(input, output) {
+#  output$druzine <- DT::renderDataTable({
+#    dcast(druzine, obcina ~ velikost.druzine, value.var = "stevilo.druzin") %>%
+#      rename(`Občina` = obcina)
+#  })
+#  
+#  output$pokrajine <- renderUI(
+#    selectInput("pokrajina", label="Izberi pokrajino",
+#                choices=c("Vse", levels(obcine$pokrajina)))
+#  )
+#  output$naselja <- renderPlot({
+#    main <- "Pogostost števila naselij"
+#    if (!is.null(input$pokrajina) && input$pokrajina %in% levels(obcine$pokrajina)) {
+#      t <- obcine %>% filter(pokrajina == input$pokrajina)
+#      main <- paste(main, "v regiji", input$pokrajina)
+#    } else {
+#      t <- obcine
+#    }
+#    ggplot(t, aes(x = naselja)) + geom_histogram() +
+#      ggtitle(main) + xlab("Število naselij") + ylab("Število občin")
+#  })
+#})
+
--- shiny/ui.R
+++ shiny/ui.R
@@ -1,17 +1,35 @@
 library(shiny)
-library(DT)
+
 shinyUI(fluidPage(
   
-  titlePanel("Slovenske občine"),
+  titlePanel("Število selitev"),
   
   tabsetPanel(
-      tabPanel("Velikost družine",
-               DT::dataTableOutput("druzine")),
-      
-      tabPanel("Število naselij",
-               sidebarPanel(
-                  uiOutput("pokrajine")
-                ),
-               mainPanel(plotOutput("naselja")))
+    tabPanel("Število priseljenih",
+             sidebarPanel(
+               selectInput("drzava",
+                           label = "Izberite državo",
+                           choices = sort(unique(tabela2$Drzava_drzavljanstva)),
+                           selected = "Slovenija"),
+               selectInput("spol",
+                           label = "Izberite spol",
+                           choices = sort(unique(tabela2$Spol))
+               )
+             ),
+             mainPanel(plotOutput("tabela2"))
+    ),
+    tabPanel("Število odseljenih",
+             sidebarPanel(
+               selectInput("drzava",
+                           label = "Izberite državo",
+                           choices = sort(unique(html1$Drzava_prihodnjega_prebivalisca))
+                           ),
+               selectInput("spol",
+                           label = "Izberite spol",
+                           choices = sort(unique(html1$Spol))
+               )
+             ),
+             mainPanel(plotOutput("html1"))
     )
+  )
 ))
